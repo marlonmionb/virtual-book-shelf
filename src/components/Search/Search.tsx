d856a3cdd24891af80039820de79087daeb27cd5
marlonmionb <marlonmionb@gmail.com>
2024-04-27 21:12:22 -0300

    4 - Search Component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,41 @@
+import { FC, useState, ChangeEvent } from "react"
+
+interface SearchProps {
+    input?: string;
+    placeholder?: string;
+    onChangeInputData?(data: string): void; // Adicionando '?' torna a propriedade opcional
+}
+
+const Search: FC<SearchProps> = ({input, placeholder, onChangeInputData}) => {
+    const [inputData, setInputData] = useState(input || '');
+
+    function onInputChange(e: ChangeEvent<HTMLInputElement>) {
+        e.preventDefault();
+        setInputData(e.target.value);
+        onChangeInputData ? onChangeInputData(e.target.value): null;
+    }
+
+    return (
+        <>
+            <input 
+                type="text" 
+                placeholder={placeholder}
+                onChange={onInputChange}
+                value={inputData}
+                className="
+                    focus:outline-none 
+                    rounded-lg 
+                    bg-gray-100
+                    text-gray-500 
+                    placeholder-gray-500 
+                    my-4 mx-4 
+                    py-1 ps-4
+                    w-1/5
+                    h-10
+                "
+            />
+        </>
+    )
+}
+
+export default Search;
