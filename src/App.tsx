59491f52aa6d5a2d8ab12c94976dc949359c198c
LucasJann <lucas.jan.jobs@gmail.com>
2024-04-30 14:05:37 -0300

    Removed the extra dependency, removed the Button component and its import from App.jsx, and made some tweaks to the Button properties.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { Fragment } from "react/jsx-runtime"
-import Button from "./components/Button/Button"
 
 function App() {
 
-const consoleLog = () => {
-  console.log('The button has being created ')
-}
-
   return (
     <Fragment>
-        <Button onClick={consoleLog}>+ New Book</Button>
     </Fragment>
   )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,20 @@
 import React, { ReactNode } from "react";
 
-
 interface ButtonProps {
   onClick: () => void;
-  type?: "submit" | "reset" | "button"
+  type?: "submit" | "reset" | "button";
   children?: ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = (props) => {
+const Button: React.FC<ButtonProps> = ({ onClick, children, type='button' }) => {
   return (
     <div>
       <button
         className="pointer-events-auto ml-8 mt-8 rounded-md bg-gray-100 px-2 py-1 text-[0.8125rem] font-semibold text-gray-400 hover:bg-gray-200 hover:text-white"
-        type={props.type || 'button'}
-        onClick={props.onClick}
+        onClick={onClick}
+        type={type}
       >
-        {props.children}
+        {children}
       </button>
     </div>
   );
