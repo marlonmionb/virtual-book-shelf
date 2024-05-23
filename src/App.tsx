3caf966e49a9bcbb698258bf58a3e2861b9b8b3e
LucasJann <lucas.jan.jobs@gmail.com>
2024-05-23 13:40:07 -0300

    Removed example.png from the Images folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react/jsx-runtime";
-
+import UserBar from "./components/UserBar/UserBar";
+import path from "./Images/example.png";
 
 function App() {
   return (
     <Fragment>
+      <UserBar path={path} alternative='User Profile'>Lucas</UserBar>
     </Fragment>
   );
 }
