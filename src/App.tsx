60dce0e011591d8bfcafbf603b3334659cdf2737
LucasJann <lucas.jan.jobs@gmail.com>
2024-05-23 13:45:05 -0300

    Tweak

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { Fragment } from "react/jsx-runtime";
-import UserBar from "./components/UserBar/UserBar";
-import path from "./Images/example.png";
 
 function App() {
   return (
     <Fragment>
-      <UserBar path={path} alternative='User Profile'>Lucas</UserBar>
     </Fragment>
   );
 }
