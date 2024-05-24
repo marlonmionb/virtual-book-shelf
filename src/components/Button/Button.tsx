6f67418dbb75e7536bc99d668e0317fa29793f9c
LucasJann <lucas.jan.jobs@gmail.com>
2024-05-23 21:53:38 -0300

    Fixed button's margin

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ onClick, children, type='button' }) => 
   return (
     <div>
       <button
-        className="pointer-events-auto ml-8 mt-8 rounded-md bg-gray-100 px-2 py-1 text-[0.8125rem] font-semibold text-gray-400 hover:bg-gray-200 hover:text-white"
+        className="pointer-events-auto ml-1 mt-2 mb-2 rounded-md bg-gray-100 px-2 py-1 text-[0.8125rem] font-semibold text-gray-400 hover:bg-gray-200 hover:text-white"
         onClick={onClick}
         type={type}
       >
