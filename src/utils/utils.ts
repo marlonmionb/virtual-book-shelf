f12e36340909185e8ad91dc6930c22371d0ff3b4
marlonmionb <marlonmionb@gmail.com>
2024-08-15 23:01:01 -0300

    Add initial tags

---
+++ src/utils/utils.ts
@@ -0,0 +1,5 @@
+export const INITIAL_TAGS = [
+    { id: "1723683995273", value: "Finance", color: "#F4CBE4" },
+    { id: "1723683956182", value: "Romance", color: "#FFBDBD" },
+    { id: "1723683643136", value: "Self Development", color: "#FCEBAF" },
+]
