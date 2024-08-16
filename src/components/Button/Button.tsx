adab9ebdf5c52191118b90a4f6df250c4779f30a
marlonmionb <marlonmionb@gmail.com>
2024-08-15 22:58:42 -0300

    Add two different styles for Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,15 @@ interface ButtonProps {
   onClick: () => void;
   type?: "submit" | "reset" | "button";
   children?: ReactNode;
+  style?: "primary" | "secondary"
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, type='button' }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, children, type='button', style="primary" }) => {
+  const buttonStyle = style === 'primary' ? 'bg-gray-200' : '';
   return (
     <div>
       <button
-        className="pointer-events-auto ml-1 mt-2 mb-2 rounded-md bg-gray-100 px-2 py-1 text-[0.8125rem] font-semibold text-gray-400 hover:bg-gray-200 hover:text-white"
+        className={`pointer-events-auto ml-1 mt-2 mb-2 rounded-md ${buttonStyle} px-2 py-1 text-[0.8125rem] text-gray-600 hover:bg-gray-200 hover:text-white`}
         onClick={onClick}
         type={type}
       >
